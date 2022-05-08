b8e8efd58443644e307fcc1f7fb1690572d38581
Kaci <kaci.yan@outlook.com>
2022-05-08 02:45:25 +0100

    Missed the auth session service for some reason

--- src/main/webapp/app/core/auth/auth-session.service.ts
+++ src/main/webapp/app/core/auth/auth-session.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie';
 
 import { SERVER_API_URL } from 'app/app.constants';
 
 @Injectable({ providedIn: 'root' })
 export class AuthServerProvider {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+    // Get xsrf token from spring security cookie
+    csrfToken: string = this.cookieService.get('XSRF-TOKEN');
 
     login(credentials): Observable<any> {
         const data =
@@ -18,7 +22,7 @@ export class AuthServerProvider {
             '&remember-me=' +
             credentials.rememberMe +
             '&submit=Login';
-        const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('X-XSRF-TOKEN', this.csrfToken);
 
         return this.http.post(SERVER_API_URL + 'api/authentication', data, { headers });
     }
